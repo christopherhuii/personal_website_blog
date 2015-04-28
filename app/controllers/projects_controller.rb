93b7343c9f7a69d51cb51add3c7e0ff24ebbdeea
christopherhuii <christopher.kam.hui@gmail.com>
2015-04-27 17:45:39 -0700

    project controller is setup with proper validations. added user_id migration for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,49 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @project = current_user.projects.build
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :github_url, :project_url)
+
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427185140) do
+ActiveRecord::Schema.define(version: 20150428004416) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150427185140) do
     t.string   "project_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
