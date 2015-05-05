186a046a300f6f0edc1149acc0454a8f0a79aacc
christopherhuii <christopher.kam.hui@gmail.com>
2015-05-05 12:13:40 -0700

    aliased the user devise routes so only master admin can access

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :project_attachments
 
-  devise_for :users
+  devise_for :users, :path => 'asdfasdf'
   resources :posts do
     resources :comments
   end
